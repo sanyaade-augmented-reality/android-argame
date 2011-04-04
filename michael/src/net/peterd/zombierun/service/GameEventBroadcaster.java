780c052eeebdcfcb7ea1d3a59299afd7fc8f5323
mikmitch1@gmail.com <mikmitch1@gmail.com@c3b91e99-90d2-04bd-338d-6a82a0f244ab>
2011-04-04 22:26:05 +0000

    Deleted obsolete experimental branch

--- michael/src/net/peterd/zombierun/service/GameEventBroadcaster.java
+++
@@ -1,8 +0,0 @@
-package net.peterd.zombierun.service;
-
-import net.peterd.zombierun.game.GameEvent;
-
-public interface GameEventBroadcaster {
-  
-  public void broadcastEvent(GameEvent event);
-}
